3abfb8d517b78664f92065950070b02df3262106
Chams91 <sunnychamsever@gmail.com>
2019-03-04 00:40:16 +0100

    extending Users tables + creating a company table

---
+++ PMTool-SaaS/Models/Company.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PMTool_SaaS.Models
+{
+    public class Company
+    {
+
+        public int CompanyId { get; set; }
+
+        public string Name { get; set; }
+
+        public string ImageUrl { get; set; }
+
+        public virtual ICollection<ApplicationUser> Users { get; set; }
+    }
+}
--- PMTool-SaaS/Models/IdentityModels.cs
+++ PMTool-SaaS/Models/IdentityModels.cs
@@ -1,4 +1,5 @@
-﻿using System.Data.Entity;
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
 using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Identity;
@@ -9,6 +10,17 @@ namespace PMTool_SaaS.Models
     // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit http://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
     public class ApplicationUser : IdentityUser
     {
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public int CompanyId { get; set; }
+
+        //prop de navigation
+        [ForeignKey("CompanyId")]
+        public virtual Company Company { get; set; }
+
         public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
         {
             // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
@@ -29,5 +41,7 @@ namespace PMTool_SaaS.Models
         {
             return new ApplicationDbContext();
         }
+
+        public DbSet<Company> Companies { get; set; }
     }
 }
